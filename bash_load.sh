c306978c54b2271d97a718875802b50fbd205a50
Josephine M Ho <josephine.m.ho@jpl.nasa.gov>
2018-12-10 22:32:36 +0000

    mkdir parent directory for nonexistent /etc/profile.d/

--- bash_load.sh
+++ bash_load.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cp .bash_profile ~/.bash_profile
-cp motd.sh ~/etc/profile.d/
+mkdir -p ~/etc/profile.d/ && cp motd.sh ~/etc/profile.d/
 source ~/.bash_profile ~/etc/profile.d
 cd ~
