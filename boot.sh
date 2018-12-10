2ba6486e33b1c8005a7932f1475365dcdcbb2abe
Josephine M Ho <josephine.m.ho@jpl.nasa.gov>
2018-12-10 21:17:20 +0000

    added chmod +x to bash_load

--- boot.sh
+++ boot.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+chmod +x bash_load.sh
 ./bash_load.sh
-cd
 curl -sSL https://get.docker.com | sh
+sudo systemctl start docker
 sudo docker pull jupyter/datascience-notebook
+sudo usermod -aG docker jmho
