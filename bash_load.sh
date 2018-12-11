1d16591a1eb1c1a3d4d04fb13052bc3c332bb657
Josephine M Ho <josephine.m.ho@jpl.nasa.gov>
2018-12-11 02:20:42 +0000

    added sudo to move font

--- bash_load.sh
+++ bash_load.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cp .bash_profile ~/.bash_profile
-cp alligator2.flf /usr/share/figlet/alligator2.flf
+sudo cp alligator2.flf /usr/share/figlet/alligator2.flf
 sudo cp motd.sh /etc/profile.d/
 source ~/.bash_profile ~/etc/profile.d
 cd ~
