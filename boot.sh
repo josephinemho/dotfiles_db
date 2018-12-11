f646baa5bfd839e3c36600565be7fcdeb5fc6d3e
Josephine M Ho <josephine.m.ho@jpl.nasa.gov>
2018-12-11 18:12:41 +0000

    added datascience stuff

--- boot.sh
+++ boot.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
+
+# Install and load environment
 sudo chmod +x bash_load.sh
 ./bash_load.sh
+sudo yum update -y
+sudo yum install -y figlet 
+
+# Install LinuxBrew
+sudo yum groupinstall -y 'Development Tools' && sudo yum install curl file git
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+
+# Install Git Large File Storage
+sudo brew install git-lfs -y 
+
+# Install Docker
 curl -sSL https://get.docker.com | sh
 sudo systemctl start docker
 sudo docker pull josephinemho/custom_dockerfile
 sudo usermod -aG docker jmho
-sudo yum update
-sudo yum install -y figlet 
+
+# Set up datascience environment
+# Clone datascience repo
+git clone https://github.jpl.nasa.gov/jmho/xmatters.git
+
+# Use Git Large File Storage for .csv and .tar files
+cd xmatters
+git lfs track "data/ps_files/*"
+git lfs track "data/picked_data/*"
