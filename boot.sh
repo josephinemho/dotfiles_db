18aead8577f5ca4e2a13b41815f3c82b691bf8ea
Josephine M Ho <josephine.m.ho@jpl.nasa.gov>
2018-12-18 01:27:45 +0000

    commented out dependency tools for git largefile storage

--- boot.sh
+++ boot.sh
@@ -7,14 +7,14 @@ sudo yum update -y
 sudo yum install -y figlet 
 sudo cp dotfiles/alligator2.flf /usr/share/figlet/alligator2.flf
 
-# Install LinuxBrew
-sudo yum groupinstall -y 'Development Tools' && sudo yum install -y curl file git
-echo | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.bash_profile
-echo 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"' >>~/.bash_profile
-echo 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"' >>~/.bash_profile
-PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-source ~/.bash_profile
+## Install LinuxBrew
+#sudo yum groupinstall -y 'Development Tools' && sudo yum install -y curl file git
+#echo | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+#echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >>~/.bash_profile
+#echo 'export MANPATH="/home/linuxbrew/.linuxbrew/share/man:$MANPATH"' >>~/.bash_profile
+#echo 'export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:$INFOPATH"' >>~/.bash_profile
+#PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+#source ~/.bash_profile
 
 # Install Docker
 curl -sSL https://get.docker.com | sh
