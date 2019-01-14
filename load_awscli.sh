ef7e1d844b61d2cbd1583bd29c1cd59b64397987
Josephine M Ho <josephine.m.ho@jpl.nasa.gov>
2019-01-14 19:56:37 +0000

    removed testdata portion for demo

--- load_awscli.sh
+++ load_awscli.sh
@@ -12,7 +12,7 @@ sudo git clone https://github.jpl.nasa.gov/cloud/Access-Key-Generation.git
 cd Access-Key-Generation/
 sudo sed -i 's/#!python/#!\/usr\/bin\/python/' aws-login*.py
 ./aws-login-gov.py
-#mkdir -p /home/jmho/work/xmatters/original_data
-#aws --profile saml-gov s3 sync s3://jmho/xmatters/original_data/ /home/jmho/work/xmatters/original_data/
-mkdir -p /root/test_data
-aws --profile saml-gov s3 sync s3://jmho/xmatters/test_data /root/test_data
+mkdir -p /home/jmho/work/xmatters/original_data
+aws --profile saml-gov s3 sync s3://jmho/xmatters/original_data/ /home/jmho/work/xmatters/original_data/
+#mkdir -p /root/data
+#aws --profile saml-gov s3 sync s3://jmho/xmatters /root/data
