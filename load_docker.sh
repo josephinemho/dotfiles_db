f25bc7db6c1806f06357bd5dd6afcf3f1dc8af45
Josephine M Ho <josephine.m.ho@jpl.nasa.gov>
2019-01-14 22:31:43 +0000

    cd into work

--- load_docker.sh
+++ load_docker.sh
@@ -4,9 +4,9 @@ sudo curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker jmho
 sudo systemctl start docker
 sudo docker pull josephinemho/custom_dockerfile
-cd
+cd work/
 sudo git clone https://github.jpl.nasa.gov/jmho/xmatters.git
 cd xmatters
 sudo docker run -it -d -p 8889:8888 -v `pwd`:/home/jovyan/work --name xmatters josephinemho/custom_dockerfile
-cd ..
+cd ~ 
 
