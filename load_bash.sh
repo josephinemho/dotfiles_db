249229e9d0315ce1d7e41721e5ce9e031a33f939
root <root@jmho-datascience.jpl.nasa.gov>
2019-01-25 10:06:04 -0800

    install tmux

--- load_bash.sh
+++ load_bash.sh
@@ -4,5 +4,6 @@ cp sources/.bash_profile ~/.bash_profile
 sudo cp sources/motd.sh /etc/profile.d/
 source ~/.bash_profile ~/etc/profile.d
 sudo yum update -y
+sudo yum install -y tmux
 sudo yum install -y figlet
 sudo cp sources/alligator2.flf /usr/share/figlet/alligator2.flf
