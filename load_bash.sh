dc5d7488e5291aaf9cea2a06dd01dd802b76d078
root <root@jmho-datascience.jpl.nasa.gov>
2019-06-03 16:06:48 -0700

    added vimrc

--- load_bash.sh
+++ load_bash.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-cp sources/.bash_profile ~/.bash_profile
+cp sources/bash_profile ~/.bash_profile
+cp sources/vimrc ~/.vimrc
 sudo cp sources/motd.sh /etc/profile.d/
-source ~/.bash_profile ~/etc/profile.d
+source ~/.bash_profile ~/etc/profile.d ~/.vimrc
 sudo yum update -y
 sudo yum install -y tmux
 sudo yum install -y figlet
