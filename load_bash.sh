abf62e3528b09d696eaedb0784066992f96053a0
Josephine M. Ho <jmho@jpl.nasa.gov>
2019-06-05 13:43:22 -0700

    install oh-my-zsh first

--- load_bash.sh
+++ load_bash.sh
@@ -4,12 +4,12 @@
 #cp sources/vimrc ~/.vimrc
 #source ~/.vimrc
 
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo cp sources/bash_profile ~/.bash_profile
 sudo cp sources/motd.sh /etc/profile.d/
 sudo cp sources/zshrc ~/.zshrc
 source ~/.bash_profile ~/etc/profile.d
 
 sudo yum update -y
-sudo yum install -y zsh tmux figlet
+sudo yum install -y tmux figlet
 sudo cp sources/alligator2.flf /usr/share/figlet/alligator2.flf
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
