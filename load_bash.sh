082cbf4572bc0f979f02af9127faae148a9ed8a1
Josephine M. Ho <jmho@jpl.nasa.gov>
2019-06-05 15:10:35 -0700

    cleaned vimrc

--- load_bash.sh
+++ load_bash.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-# Use zsh instead of vimrc
-#cp sources/vimrc ~/.vimrc
-#source ~/.vimrc
-
+# Install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y 
 # Clone the zsh-syntax-highlighting plugin’s repo and copy it to the “Oh My ZSH” plugins directory.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# Copy source files to locations
+sudo cp sources/vimrc ~/.vimrc
 sudo cp sources/bash_profile ~/.bash_profile
-sudo cp sources/motd.sh /etc/profile.d/
+sudo cp sources/motd.sh /etc/profile.d/motd.sh
 sudo cp sources/zshrc ~/.zshrc
-source ~/.bash_profile ~/etc/profile.d
+source ~/.bash_profile ~/etc/profile.d ~/.zshrc
 
 sudo yum update -y
 sudo yum install -y tmux figlet
