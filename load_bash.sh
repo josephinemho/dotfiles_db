5835979101c04d07fddd320b59d2100c1b71d6ca
Josephine M. Ho <jmho@jpl.nasa.gov>
2019-06-05 13:37:44 -0700

    updated zsh settings

--- load_bash.sh
+++ load_bash.sh
@@ -10,9 +10,6 @@ sudo cp sources/zshrc ~/.zshrc
 source ~/.bash_profile ~/etc/profile.d
 
 sudo yum update -y
-sudo yum install -y tmux figlet
+sudo yum install -y zsh tmux figlet
 sudo cp sources/alligator2.flf /usr/share/figlet/alligator2.flf
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo "/usr/bin/zsh" | sudo tee -a /etc/shells
-chsh -s $(which zsh)
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
