e6499c863d4c5d218f36b245a1acbd256ec76f58
Josephine M. Ho <jmho@jpl.nasa.gov>
2019-06-05 14:15:01 -0700

    added syntax highlighting and other plugins

--- load_bash.sh
+++ load_bash.sh
@@ -4,7 +4,10 @@
 #cp sources/vimrc ~/.vimrc
 #source ~/.vimrc
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y 
+# Clone the zsh-syntax-highlighting plugin’s repo and copy it to the “Oh My ZSH” plugins directory.
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 sudo cp sources/bash_profile ~/.bash_profile
 sudo cp sources/motd.sh /etc/profile.d/
 sudo cp sources/zshrc ~/.zshrc
