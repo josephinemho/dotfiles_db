1d0f601152f37dc32e4b2168e469a521fc4dd1ae
root <root@ip-172-31-44-116.ec2.internal>
2019-10-23 20:16:31 +0000

    uncommented out docker

--- load_docker.sh
+++ load_docker.sh
@@ -2,11 +2,11 @@
 
 #sudo curl -sSL https://get.docker.com | sh
 sudo yum -y install docker
-sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo systemctl start docker
+#sudo groupadd docker
+#sudo usermod -aG docker $USER
+#sudo systemctl start docker
 #sudo docker pull josephinemho/custom_dockerfile
 sudo docker pull jupyter/minimal-notebook
 cd ~
-sudo docker run -it -d -p 8889:8888 -v `pwd`:/home/jovyan/work --name my_container jupyter/minimal-notebook
-cd ~ 
+#sudo docker run -it -d -p 8889:8888 -v `pwd`:/home/jovyan/work --name my_container jupyter/minimal-notebook
+#cd ~ 
